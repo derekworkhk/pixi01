a0a0f89800ce906ccbfefa6d7ec10fded20d081d
HKGAD\derek.sk.chow <derek.sk.chow@hkbn.com.hk>
2021-04-28 17:08:22 +0800

    4.28

--- components/Pixi.js
+++ components/Pixi.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import * as PIXI from "pixi.js";
 import { TweenMax, Power3 } from 'gsap';
+import '../styles/Pixi.module.css'
 
 class PixiComponent extends React.Component {
     state = { width: 0, height: 0 };
@@ -15,50 +16,87 @@ class PixiComponent extends React.Component {
     }
 
     componentDidMount() {
-        this.app = new PIXI.Application(window.innerWidth, window.innerHeight,
+        let that = this
+
+        that.app = new PIXI.Application(window.innerWidth, window.innerHeight,
             {
                 backgroundColor: 0xFFFFFF
             });
 
         // full screen
-        this.app.renderer.view.style.position = "absolute";
-        this.app.renderer.view.style.display = "block";
-        this.app.renderer.autoResize = true;
-        this.app.renderer.resize(window.innerWidth, window.innerHeight);
+        that.app.renderer.view.style.position = "absolute";
+        that.app.renderer.view.style.display = "block";
+        that.app.renderer.autoResize = true;
+        that.app.renderer.resize(window.innerWidth, window.innerHeight);
 
-        this.gameCanvas.appendChild(this.app.view);
+        that.gameCanvas.appendChild(this.app.view);
 
-        var style = new PIXI.TextStyle({
-            fontFamily: 'Arial',
-            fontSize: 48,
-            fontStyle: 'italic',
-            fontWeight: 'bold',
-            fill: '#ffffff',
-            wordWrap: true,
-            wordWrapWidth: 440
-        });
+        let loader = that.app.loader;
+        function loaderDone(){
+            console.log('done loader')
 
-        // let texture = PIXI.utils.TextureCache["public/logo.svg"];
-        let logo = PIXI.Sprite.from('/logo.svg')
+            let bg = new PIXI.Sprite(loader.resources["/image_02.png"].texture);
 
-        console.log(logo.width)
-        console.log(logo.height)
+            console.log('bgw '+bg.width)
+            console.log('bgh '+bg.height)
 
-        // var richText = new PIXI.Text('Rich text with a lot of options and across multiple lines', style);
-        logo.anchor.set(0.5);
-        logo.x = window.innerWidth/2;
-        logo.y = window.innerHeight/2;
+            let widthScale = (window.innerWidth/bg.width)
+            let heightScale = (window.innerWidth/bg.height)
 
-        this.app.stage.addChild(logo);
+            if (widthScale>=1&&heightScale>=1){
+                bg.scale.x=Math.min(widthScale,heightScale)
+            } else {
+                bg.scale.x=Math.max(widthScale,heightScale)
+            }
 
-        this.app.start();
 
-        TweenMax.set(logo, {alpha:0});
-        TweenMax.set(logo.scale, {x: 1.3, y: 1.3});
-        TweenMax.to(logo, 2.5, { alpha:1, ease: Power3.easeOut } );
-        TweenMax.to(logo.scale, 2.5, { x: 1, y: 1, ease: Power3.easeOut} );
+            bg.x=0;
+            bg.y=window.innerHeight;
+            bg.anchor.x=0;
+            bg.anchor.y=1;
+            that.app.stage.addChild(bg);
 
-        window.addEventListener('resize', this.updateDimensions);
+            var style = new PIXI.TextStyle({
+                fontFamily: 'mcsaatchi',
+                fontSize: 48,
+                fontStyle: 'normal',
+                fontWeight: 'bold',
+                fill: '#ffffff',
+                wordWrap: true,
+                wordWrapWidth: 440
+            });
+            var text1 = new PIXI.Text('HOLD MY BEER. CUDDLING WITH MY CAT', style);
+            text1.x = window.innerWidth/2;
+            text1.y = window.innerHeight/2;
+            // text1.alpha=0;
+            that.app.stage.addChild(text1);
+
+            // let logo = PIXI.Sprite.from('/logo.svg')
+            let logo = new PIXI.Sprite(loader.resources["/logo.svg"].texture)
+            console.log('logow '+logo.width)
+            console.log('logoh '+logo.height)
+            logo.anchor.set(0.5);
+            logo.x = window.innerWidth/2;
+            logo.y = window.innerHeight/2;
+            logo.scale.x=1.3;
+            logo.scale.y=1.3;
+            logo.alpha=0;
+            that.app.stage.addChild(logo);
+
+            that.app.start();
+
+            // TweenMax.set(logo, {alpha:0});
+            TweenMax.to(logo, 2.5, { alpha:1, ease: Power3.easeOut } );
+            TweenMax.to(logo.scale, 2.5, { x: 1, y: 1, ease: Power3.easeOut} );
+
+            window.addEventListener('resize', that.updateDimensions);
+
+        }
+        loader
+            .add('/image_02.png')
+            .add('/mcsaatchiheavy.ttf')
+            .add('/logo.svg')
+            .load(loaderDone);
     }
 
     componentWillUnmount() {
